a82676ddeeea459b6546a35bf9bf888bed16fb18
manik <maniksejwal@gmail.com>
2019-11-17 16:53:35 +0530

    xception with wrapper. Does not work

--- xception.py
+++ xception.py
@@ -192,8 +192,8 @@ def Xception(img_input=None):
 
     # Create model.
     model = models.Model(img_input, x, name='xception')
-    #model.compile(optimizer='adam', loss='mean_squared_error')
-    #if weights_path != None: model.load_weights(weights_path)
+    # model.compile(optimizer='adam', loss='mean_squared_error')
+    # if weights_path != None: model.load_weights(weights_path)
 
     return model
 
@@ -242,15 +242,132 @@ def fancy_nn(weights_path=None):
 
     return model
 
+
+def state_to_inputs(state):
+    linear_inputs = [
+        int(state['equipped_items']['mainhand']['damage']),
+        int(state['equipped_items']['mainhand']['maxDamage']),
+        int(state['equipped_items']['mainhand']['type']),
+        int(state['inventory']['coal']),
+        int(state['inventory']['cobblestone']),
+        int(state['inventory']['crafting_table']),
+        int(state['inventory']['dirt']),
+        int(state['inventory']['furnace']),
+        int(state['inventory']['iron_axe']),
+        int(state['inventory']['iron_ingot']),
+        int(state['inventory']['iron_ore']),
+        int(state['inventory']['iron_pickaxe']),
+        int(state['inventory']['log']),
+        int(state['inventory']['planks']),
+        int(state['inventory']['stick']),
+        int(state['inventory']['stone']),
+        int(state['inventory']['stone_axe']),
+        int(state['inventory']['stone_pickaxe']),
+        int(state['inventory']['torch']),
+        int(state['inventory']['wooden_axe']),
+        int(state['inventory']['wooden_pickaxe'])
+    ]
+
+    img_input = state['pov']
+
+    return [linear_inputs, img_input]
+
+
+def label_to_output(labels):
+    binary_labels = []
+    binary_labels.append(labels['attack'])
+    binary_labels.append(labels['back'])
+    binary_labels.append(labels['forward'])
+    binary_labels.append(labels['jump'])
+    binary_labels.append(labels['left'])
+    binary_labels.append(labels['right'])
+    binary_labels.append(labels['sneak'])
+    binary_labels.append(labels['sprint'])
+
+    linear_labels = []
+    linear_labels.append(int(labels['camera'][0]))
+    linear_labels.append(int(labels['camera'][1]))
+    linear_labels.append(labels['craft'])
+    linear_labels.append(labels['equip'])
+    linear_labels.append(labels['nearbyCraft'])
+    linear_labels.append(labels['nearbySmelt'])
+    linear_labels.append(labels['place'])
+
+    return [binary_labels, linear_labels]
+
+
+def outputs_to_action(outputs):
+    binary_labels = outputs[0]
+    linear_labels = outputs[1]
+
+    from collections import OrderedDict
+    from numpy import array, float32
+
+    action = OrderedDict({
+        'attack': binary_labels[0],
+        'back': binary_labels[1],
+        'camera': array([linear_labels[0][0], linear_labels[0][1]], dtype=float32),
+        'craft': linear_labels[1],
+        'equip': linear_labels[2],
+        'forward': binary_labels[2],
+        'jump': binary_labels[3],
+        'left': binary_labels[4],
+        'nearbyCraft': linear_labels[3],
+        'nearbySmelt': linear_labels[4],
+        'place': linear_labels[5],
+        'right': binary_labels[5],
+        'sneak': binary_labels[6],
+        'sprint': binary_labels[7]
+    })
+
+    return action
+
+
 if __name__ == '__main__':
     model = fancy_nn()
-    from keras.utils import plot_model
 
-    plot_model(model, to_file='model.png')
+    # from keras.utils import plot_model
+    # plot_model(model, to_file='model.png')
+
+    inputs = state_to_inputs(minerl.env.obtain_observation_space.sample())
+    labels = label_to_output(minerl.env.obtain_action_space.sample())
+
+    # print(len(inputs))
+    # print(inputs)
+    # print(len(inputs[0]))
+    # print('\n')
+    # print(inputs[0])
+    # print('\n')
+    # print(len(inputs[1]))
+    # print('\n')
+    # print(inputs[1])
+    # print('\n')
+
+    predictions = model.predict(inputs)
+    # action = outputs_to_action(predictions)
+    # print(action)
+
+    # print(minerl.env.obtain_action_space.sample())
     # Xception(minerl.env.obtain_observation_space, minerl.env.obtain_action_space.sample())
-    #for i in range(10):
+    for i in range(10):
+        pass
         # print(minerl.env.obtain_action_space.sample())
-        # [('attack', 1), ('back', 1), ('camera', array([134.4457 , 173.21553], dtype=float32)), ('craft', 1), ('equip', 5), ('forward', 1), ('jump', 0), ('left', 0), ('nearbyCraft', 3), ('nearbySmelt', 0), ('place', 1), ('right', 0), ('sneak', 1), ('sprint', 0)]
+        """[
+            ('attack', 1),
+            ('back', 1),
+            ('camera', array([134.4457 , 173.21553], dtype=float32)),
+            ('craft', 1),
+            ('equip', 5),
+            ('forward', 1),
+            ('jump', 0),
+            ('left', 0),
+            ('nearbyCraft', 3),
+            ('nearbySmelt', 0),
+            ('place', 1),
+            ('right', 0),
+            ('sneak', 1),
+            ('sprint', 0)
+        ]"""
         # [('attack', 1), ('back', 1), ('camera', array([-11.986441, -67.31623 ], dtype=float32)), ('craft', 0), ('equip', 0), ('forward', 1), ('jump', 0), ('left', 1), ('nearbyCraft', 5), ('nearbySmelt', 2), ('place', 3), ('right', 1), ('sneak', 0), ('sprint', 0)]
         # [('attack', 1), ('back', 1), ('camera', array([ 114.31422, -140.80772], dtype=float32)), ('craft', 4), ('equip', 5), ('forward', 0), ('jump', 1), ('left', 0), ('nearbyCraft', 7), ('nearbySmelt', 1), ('place', 2), ('right', 1), ('sneak', 1), ('sprint', 1)]
         # [('attack', 0), ('back', 0), ('camera', array([  71.87382, -169.42906], dtype=float32)), ('craft', 4), ('equip', 3), ('forward', 1), ('jump', 0), ('left', 1), ('nearbyCraft', 7), ('nearbySmelt', 0), ('place', 2), ('right', 1), ('sneak', 1), ('sprint', 0)]
@@ -261,6 +378,39 @@ if __name__ == '__main__':
         # [('attack', 0), ('back', 0), ('camera', array([ 38.89587, 148.0062 ], dtype=float32)), ('craft', 0), ('equip', 6), ('forward', 1), ('jump', 1), ('left', 1), ('nearbyCraft', 2), ('nearbySmelt', 1), ('place', 0), ('right', 0), ('sneak', 0), ('sprint', 1)]
         # [('attack', 1), ('back', 0), ('camera', array([158.30092 ,  84.317894], dtype=float32)), ('craft', 4), ('equip', 3), ('forward', 1), ('jump', 0), ('left', 0), ('nearbyCraft', 4), ('nearbySmelt', 2), ('place', 1), ('right', 1), ('sneak', 0), ('sprint', 0)]
         # [('attack', 0), ('back', 0), ('camera', array([-135.29819,  -26.28303], dtype=float32)), ('craft', 3), ('equip', 0), ('forward', 1), ('jump', 1), ('left', 1), ('nearbyCraft', 3), ('nearbySmelt', 2), ('place', 5), ('right', 0), ('sneak', 1), ('sprint', 0)]
-        #print(minerl.env.obtain_observation_space.sample())
-        # [('equipped_items', OrderedDict([('mainhand', OrderedDict([('damage', array(291)), ('maxDamage', array(136)), ('type', 7)]))])), ('inventory', OrderedDict([('coal', array(1777)), ('cobblestone', array(2185)), ('crafting_table', array(926)), ('dirt', array(2150)), ('furnace', array(1135)), ('iron_axe', array(547)), ('iron_ingot', array(446)), ('iron_ore', array(1711)), ('iron_pickaxe', array(984)), ('log', array(1037)), ('planks', array(1337)), ('stick', array(1221)), ('stone', array(713)), ('stone_axe', array(315)), ('stone_pickaxe', array(2070)), ('torch', array(364)), ('wooden_axe', array(1056)), ('wooden_pickaxe', array(2145))]))]
+
+        # print(minerl.env.obtain_observation_space.sample())
+        """[
+            ('equipped_items', OrderedDict(
+                [('mainhand', OrderedDict(
+                        [
+                            ('damage', array(291)),
+                            ('maxDamage', array(136)),
+                            ('type', 7)
+                        ]
+                ))]
+            )),
+            ('inventory', OrderedDict(
+                [
+                    ('coal', array(1777)),
+                    ('cobblestone', array(2185)),
+                    ('crafting_table', array(926)),
+                    ('dirt', array(2150)),
+                    ('furnace', array(1135)),
+                    ('iron_axe', array(547)),
+                    ('iron_ingot', array(446)),
+                    ('iron_ore', array(1711)),
+                    ('iron_pickaxe', array(984)),
+                    ('log', array(1037)),
+                    ('planks', array(1337)),
+                    ('stick', array(1221)),
+                    ('stone', array(713)),
+                    ('stone_axe', array(315)),
+                    ('stone_pickaxe', array(2070)),
+                    ('torch', array(364)),
+                    ('wooden_axe', array(1056)),
+                    ('wooden_pickaxe', array(2145))
+                ]
+            ))
+        ]"""
         # 21 items
